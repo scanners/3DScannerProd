72e892495ff53d7cb26bacf9c332f51262c032ba
Tyler Gunter <guntertm@plu.edu>
2013-04-16 16:59:28 -0700

    Work on Qt Style Sheets

--- 3DScannerApp/3DScannerApp/main.cpp
+++ 3DScannerApp/3DScannerApp/main.cpp
@@ -4,6 +4,25 @@ int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
     // instantatiate our main window here
+
+	a.setStyleSheet("QPushButton {"
+			"background-color: #333;"
+			"color : white;"
+			"border-style: outset;"
+			"border-width: 2px;"
+			"border-radius: 10px;"
+			"border-color: white;"
+			"font: bold 14px;"
+			"min-width: 5em;"
+			"padding: 4px;}"
+			"QPushButton:hover {"
+			"background-color: #666;}"
+			"QLineEdit { background-color: #EEE;}"
+			"QLineEdit:hover { background-color: #FFF;}"
+			"QLineEdit:selected { background-color: #FFF;}"
+			);
+
+
 	HomeView hv;
 	hv.show();
 
